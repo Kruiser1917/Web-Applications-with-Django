2244ddb25f631d20c960e98377e0daa5bd8c9ac2
aptik <aptik@DESKTOP-SVN8V0A>
2024-10-24 13:10:42 +0300

    Удалил дублирование контроллера ProductCreateView и добавил LoginRequiredMixin

--- catalog/views.py
+++ catalog/views.py
@@ -4,40 +4,42 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView, T
 from .models import Product
 from .forms import ProductForm
 
+# Список продуктов
 class ProductListView(ListView):
     model = Product
     template_name = 'catalog/product_list.html'
     context_object_name = 'products'
 
-class ProductCreateView(CreateView):
-    model = Product
-    form_class = ProductForm
-    template_name = 'catalog/product_form.html'
-    success_url = reverse_lazy('catalog:product_list')
-
+# Создание продукта (только для авторизованных пользователей)
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
-    fields = ['name', 'price', 'available']
-    success_url = '/'
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
 
-class ProductUpdateView(UpdateView):
+# Обновление продукта (только для авторизованных пользователей)
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:product_list')
 
-class ProductDeleteView(DeleteView):
+# Удаление продукта (только для авторизованных пользователей)
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:product_list')
 
+# Главная страница
 class HomeView(TemplateView):
     template_name = 'catalog/home.html'
 
+# Страница контактов
 class ContactView(TemplateView):
     template_name = 'catalog/contact.html'
 
+# Детали продукта
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'catalog/product_detail.html'  # Шаблон для детальной информации о продукте232
+    template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
